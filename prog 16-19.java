b6cd62a22b38f7c429c265da0e13e565f64771b4
jerlyn358 <jerlynmaenavarro6@gmail.com>
2025-10-06 18:48:50 +0800

    Create prog 16-19.java

---
+++ prog 16-19.java
@@ -0,0 +1,39 @@
+public class Program16{
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println("Hello");
+            i++;
+        }while (i <= 10);
+    }
+}
+
+public class Program17{
+    public static void main(String[] args){
+        int i =0;
+        while (i <= 10){
+            System.out.println(i + " - gimingaw nako niyaaaa:((");
+            i++;
+        }
+    }
+}
+
+public class Program18{
+    public static void main(String[] args){
+        for (int i = 10; i > 0; i--){
+            System.out.println(i + " ");
+            
+        }
+    }
+}
+
+public class Program19{
+    public static void main(String[] args){
+        for (int i = 1; i <= 5; i++){
+            for (int j = 1; j<=5; j++){
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
+}
